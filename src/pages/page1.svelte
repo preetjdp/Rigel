582cfc0394d4805638e1a8200a588094189cfe63
preetjdp <preetjdp123@gmail.com>
2020-11-26 15:55:21 +0530

    More small

--- src/pages/page1.svelte
+++ src/pages/page1.svelte
@@ -1,18 +1,25 @@
 <script lang="ts">
-
-    import Text from "../components/Text.svelte"
-    import Button from "../components/Button.svelte"
+    import Text from "../components/Text.svelte";
+    import Button from "../components/Button.svelte";
 </script>
 
-
-<div>
-    <Text>Tell us a bit about yourself</Text>
-    <Button type="primary">Proceed</Button>
-</div>
-
 <style>
-    div {
+    .parent-container {
         width: 80%;
         text-align: center;
     }
+
+    .bottom-row {
+        display: flex;
+        align-items: center;
+        justify-content: space-around;
+    }
 </style>
+
+<div class="parent-container">
+    <Text>Tell us a bit about yourself</Text>
+    <div class="bottom-row">
+        <Button type="secondary" disabled>Back</Button>
+        <Button type="primary">Proceed</Button>
+    </div>
+</div>
